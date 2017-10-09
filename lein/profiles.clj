3697e444910e412e3efa13b834797a537347eb6b
kawas44 <kawas44@gmail.com>
2017-10-09 14:26:23 +0200

    Update libs in lein user profile

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -3,19 +3,19 @@
  {:dependencies [ ;;debugging [redl "0.2.4"]
                  [slamhound "1.5.5"]
                  [alembic "0.3.2"]
-                 [cljfmt "0.5.6"]
+                 [cljfmt "0.5.7"]
                  [spyscope "0.1.6"]
-                 [org.clojure/tools.nrepl "0.2.12"]]
+                 [org.clojure/tools.nrepl "0.2.13"]]
 
   :injections [   ;;debugging (require '[redl complete core])
                (require 'spyscope.core)]
 
-  :plugins [[cider/cider-nrepl "0.13.0"]
+  :plugins [[cider/cider-nrepl "0.15.1"]
             [lein-pprint "1.1.2"]
-            [lein-cljfmt "0.5.6"]
+            [lein-cljfmt "0.5.7"]
             ;;[com.palletops/lein-shorthand "0.4.0"]
             ]
 
-  ;;:shorthand {. [^:lazy alembic.still/load-project
-  ;;               ^:lazy ^:macro alembic.still/lein]}
+  :aliases  {"slamhound"  ["run" "-m" "slam.hound"]}
+
   }}
