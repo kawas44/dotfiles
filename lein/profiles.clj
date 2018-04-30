ad96396b8fd81b9343e14fe73b9374ddce16645d
kawas44 <kawas44@gmail.com>
2018-04-30 12:06:00 +0200

    lein: better user libs

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,54 +1,62 @@
 {:user
 
- {:dependencies [ ;;debugging [redl "0.2.4"]
-                 [alembic "0.3.2"]
+ {:dependencies [;; Begin dependency resolution
+                 [org.clojure/tools.nrepl "0.2.13" :exclusions [org.clojure/clojure]]
+                 ;; End
+
+                 ;; REPL tooling
+                 [zcaudate/lucid.core.inject "1.4.4"]
+
+                 ;; Debug
+                 [spyscope "0.1.6"]
                  [aprint "0.1.3"]
-                 [cljfmt "0.5.7"]
-                 [criterium "0.4.4"]
-                 [im.chit/lucid.core.inject "1.3.13"]
-                 [im.chit/lucid.core.namespace "1.3.13"]
-                 [inspector-jay "0.3"]
+                 [zcaudate/lucid.core.debug "1.4.4"]
+                 [inspector-jay "0.3" :exclusions [org.clojure/core.memoize]]
                  [jsofra/data-scope "0.1.2"]
                  [org.clojure/tools.trace "0.7.9"]
+
+                 ;; Bench & Profile
+                 [criterium "0.4.4"]
+                 [com.clojure-goes-fast/clj-async-profiler "0.1.2"]
+
+                 ;; Test
                  [pjstadig/humane-test-output "0.8.3"]
-                 [slamhound "1.5.5"]
                  ]
 
-  :injections [   ;;debugging (require '[redl complete core])
-               (require 'pjstadig.humane-test-output)
-               (pjstadig.humane-test-output/activate!)
-
+  :injections [;; load reader macros
+               (require 'spyscope.core)
                (require 'data-scope.inspect)
                (require 'data-scope.pprint)
 
+               ;; activate test output reformat
+               (require 'pjstadig.humane-test-output)
+               (pjstadig.humane-test-output/activate!)
+
+               ;; inject vars
                (require '[lucid.core.inject :as inject])
                (inject/in
+                 ;; debug
                  [aprint.core :refer [aprint ap nprint np]]
+                 [lucid.core.debug :refer [dbg-> dbg->> ->doto ->>doto ->prn]]
                  [inspector-jay.core :refer [inspect]]
-                 ;[clojure.inspector :refer [inspect inspect-table inspect-tree]]
-                 [clojure.pprint :refer [pprint pp]]
-                 [clojure.tools.trace :refer [deftrace dotrace trace trace-forms]]
+                 [clojure.tools.trace]
+                 ;; bench & profile
                  [criterium.core :refer [with-progress-reporting bench quick-bench]]
-                 [lucid.core.namespace :refer [clear-aliases clear-mappings]]
-                 )
-               ]
+                 )]
 
-  :plugins [ ;;[com.palletops/lein-shorthand "0.4.0"]
-            [cider/cider-nrepl "0.15.1"]
+  :plugins [
+            [cider/cider-nrepl "0.16.0"]
             [jonase/eastwood "0.2.5"]
-            [lein-ancient "0.6.12"]
-            [lein-bikeshed "0.5.0"]
-            [lein-cloverage "1.0.9"]
+            [lein-ancient "0.6.15"]
+            [lein-bikeshed "0.5.1" :exclusions [org.clojure/tools.namespace]]
+            [lein-cloverage "1.0.10"]
             [lein-kibit "0.1.6"]
-            [lein-pprint "1.1.2"]
+            [lein-pprint "1.2.0"]
             [refactor-nrepl "2.3.1"]
+            [venantius/yagni "0.1.4"]
             ]
 
-  :repl-options {:init (require 'cljfmt.core)}
-
   :jvm-opts ["-XX:-OmitStackTraceInFastThrow" "-XX:+CMSClassUnloadingEnabled"]
-
-  :cljfmt {:remove-consecutive-blank-lines? false}
 
   :bikeshed {:max-line-length 180
              :trailing-whitespace false
