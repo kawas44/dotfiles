962bf766df71f9a78cca29b2b373af4c070a9b43
kawas44 <kawas44@gmail.com>
2019-08-13 17:18:44 +0200

    lein: major changes in user profile

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,7 +1,9 @@
 {:user
 
  {:dependencies [;; Begin dependency resolution
-                 [org.clojure/tools.nrepl "0.2.13" :exclusions [org.clojure/clojure]]
+                 [nrepl "0.6.0"]
+                 [cider/cider-nrepl "0.21.1"]
+                 [refactor-nrepl "2.4.0"]
                  ;; End
 
                  ;; REPL tooling
@@ -9,25 +11,19 @@
 
                  ;; Debug
                  [spyscope "0.1.6"]
-                 ; [aprint "0.1.3"]
-                 [zcaudate/lucid.core.debug "1.4.7"]
                  [inspector-jay "0.3" :exclusions [org.clojure/core.memoize]]
                  [vvvvalvalval/scope-capture "0.3.2"]
-                 [jsofra/data-scope "0.1.2"]
-                 ; [org.clojure/tools.trace "0.7.10"]
 
                  ;; Bench & Profile
-                 [criterium "0.4.4"]
-                 ; [com.clojure-goes-fast/clj-async-profiler "0.1.3"]
+                 #_[criterium "0.4.4"]
+                 #_[com.clojure-goes-fast/clj-async-profiler "0.1.3"]
 
                  ;; Test
-                 [pjstadig/humane-test-output "0.8.3"]
-                 ]
+                 [pjstadig/humane-test-output "0.9.0"]]
 
-  :injections [;; load reader macros
+  :injections [(clojure.core/require '[clojure.core :refer :all])
+               ;; load reader macros
                (require 'spyscope.core)
-               (require 'data-scope.inspect)
-               (require 'data-scope.pprint)
 
                ;; activate test output reformat
                (require 'pjstadig.humane-test-output)
@@ -40,38 +36,42 @@
                (require '[lucid.core.inject :as inject])
                (inject/in
                  ;; debug
-                 ; [aprint.core :refer [aprint ap nprint np]]
-                 [lucid.core.debug :refer [dbg-> dbg->> ->doto ->>doto ->prn]]
                  [inspector-jay.core :refer [inspect]]
-                 ; [clojure.tools.trace]
                  ;; bench & profile
-                 [criterium.core :refer [with-progress-reporting bench quick-bench]]
-                 )]
+                 #_[criterium.core :refer [with-progress-reporting bench quick-bench]])]
 
-  :plugins [
-            [cider/cider-nrepl "0.18.0"]
-            [jonase/eastwood "0.3.3"]
+  :plugins [[com.jakemccrary/lein-test-refresh "0.24.1"]
             [lein-ancient "0.6.15"]
-            [lein-bikeshed "0.5.1" :exclusions [org.clojure/tools.namespace]]
-            [lein-cloverage "1.0.13"]
-            [lein-eftest "0.5.3"]
-            [lein-kibit "0.1.6"]
+            [lein-cljfmt "0.6.4"]
+            [lein-cloverage "1.1.1"]
+            [lein-eftest "0.5.8"]
             [lein-pprint "1.2.0"]
-            [refactor-nrepl "2.4.0"]
-            [venantius/yagni "0.1.6"]
-            ]
+            [lein-shell "0.5.0"]]
+
 
   :jvm-opts ["-XX:-OmitStackTraceInFastThrow" "-XX:+CMSClassUnloadingEnabled"]
+  :bootclasspath false
 
-  :repl-options {:init (do (require 'refactor-nrepl.config)
-                           (alter-var-root #'refactor-nrepl.config/*config* assoc :prefix-rewriting false))
-                 }
+  :repl-options {:nrepl-middleware [cider.nrepl/wrap-classpath
+                                    cider.nrepl/wrap-complete
+                                    cider.nrepl/wrap-debug
+                                    cider.nrepl/wrap-format
+                                    cider.nrepl/wrap-info
+                                    cider.nrepl/wrap-macroexpand
+                                    cider.nrepl/wrap-ns
+                                    cider.nrepl/wrap-out
+                                    cider.nrepl/wrap-spec
+                                    cider.nrepl/wrap-test
+                                    cider.nrepl/wrap-trace
+                                    cider.nrepl/wrap-undef
+                                    refactor-nrepl.middleware/wrap-refactor]}
+
+  :test-refresh {:quiet true}
 
   :eftest {:multithread? false}
 
-  :bikeshed {:max-line-length 180
-             :trailing-whitespace false
-             :trailing-blank-lines false}
+  :cljfmt {; tonsky-style :indents ^:replace {#"^\w" [[:inner 0]]}
+           :remove-consecutive-blank-lines? false}
 
   :eastwood {:linters [:all]
              :exclude-linters [:non-clojure-file :unused-locals :keyword-typos :unused-fn-args]
